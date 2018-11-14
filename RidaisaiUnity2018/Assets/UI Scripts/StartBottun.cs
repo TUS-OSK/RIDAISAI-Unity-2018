369269ed08eebdd93d7706849a70835a9f8c6d96
幡谷亮 <hatayaryo@kaguwlan215230.ed.tus.ac.jp>
2018-11-14 10:13:43 +0900

    スタート画面，仮置き

---
+++ RidaisaiUnity2018/Assets/UI Scripts/StartBottun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBottun : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+        Debug.Log("クリックされた");
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
