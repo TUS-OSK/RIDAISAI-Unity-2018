f6cf0d72503b592e42ad6ff56d78da642f278f9e
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2018-11-17 18:27:28 +0900

    いい感じに

---
+++ RidaisaiUnity2018/Assets/Naoki/PlyerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerController : MonoBehaviour {
+
+    [SerializeField]GameObject mainChar;
+
+    GameObject insCtrl;
+
+
+    Vector3 move = new Vector3(1, 0, 1);
+	// Use this for initialization
+	void Start () {
+        insCtrl = Instantiate(mainChar);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RidaisaiUnity2018/Assets/Naoki/Testkoru.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testkoru : MonoBehaviour {
+
+
+    private Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        StartCoroutine(buttai());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private IEnumerator buttai(){
+        Debug.Log("落ちた");
+        yield return new WaitForSeconds(3);
+
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        Debug.Log("止まった");
+
+        yield return new WaitForSeconds(3);
+
+        Destroy(gameObject);
+        Debug.Log("ぶっ壊れた！");
+    }
+}
